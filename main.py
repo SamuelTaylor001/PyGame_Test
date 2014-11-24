7ce2696a066e6b0e793b654d29fe5a9184eed599
sabuntu <sammy.whythim@gmail.com>
2014-11-24 22:33:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import pygame
+
+#initializes all of the pygame modules
+pygame.init()
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+
+#Sets the canvas
+gameDisplay = pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption('Pygame test yo!')
+
+gameExit = False
+
+#The variables for the lead block
+lead_x = 300
+lead_y = 300
+lead_x_change = 0
+
+clock = pygame.time.Clock()
+
+#exit function
+def exit_game(exit_event):
+    if exit_event.type == pygame.QUIT:
+            return True
+
+#gameloop
+while not gameExit:
+    for event in pygame.event.get():
+        gameExit = exit_game(event)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                lead_x_change = -10
+            if event.key == pygame.K_RIGHT:
+                lead_x_change = 10
+
+    lead_x += lead_x_change
+
+
+
+    gameDisplay.fill(white)
+
+    gameDisplay.fill(black, rect=[lead_x, lead_y, 10, 10])
+
+    pygame.display.update()
+
+    clock.tick(60)
+
+
+pygame.quit()
+quit()
